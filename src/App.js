a39abfbbb0f0c0e9eaeae741c07a8c8c872c7a4b
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-01-22 22:30:22 -0300

    ee

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 class App extends React.Component {
     render() {
